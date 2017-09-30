6cf596d645769df67848c69a6f2e5820b8fa8a13
Matt Hough <matt@hough.tv>
2017-09-30 22:19:14 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,5 +17,6 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     person.each do |attribute, data|
+      if attribute == favorite_ice_cream_flavors.include?("strawberry")
   end
 end
